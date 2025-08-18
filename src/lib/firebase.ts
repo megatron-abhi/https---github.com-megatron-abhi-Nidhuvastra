6704dd4b9c30a2ab01b77ff0342dd1ff52f908b3
Abhilash N <abhilashn8080@gmail.com>
2025-08-18 05:57:24 +0000

    replace the firebase config with this "// Import the functions you need

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,21 +2,30 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from 'firebase/database';
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCPRiZ3pyxobPaFl_cZ9ZhKw5AbapXroM",
-  authDomain: "sareeshree.firebaseapp.com",
-  databaseURL: "https://sareeshree-default-rtdb.firebaseio.com",
-  projectId: "sareeshree",
-  storageBucket: "sareeshree.firebasestorage.app",
-  messagingSenderId: "990941697261",
-  appId: "1:990941697261:web:869140b961e547875ce8e4"
+  apiKey: "AIzaSyCYkAsDIWCEaNek2ovmtVTuFyJDJHTqfvw",
+  authDomain: "nidhuvastra-a6a86.firebaseapp.com",
+  databaseURL: "https://nidhuvastra-a6a86-default-rtdb.firebaseio.com",
+  projectId: "nidhuvastra-a6a86",
+  storageBucket: "nidhuvastra-a6a86.firebasestorage.app",
+  messagingSenderId: "236711523695",
+  appId: "1:236711523695:web:731c52797318dba741649a",
+  measurementId: "G-7BTTFDW6MC"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getDatabase(app);
+// Conditionally initialize analytics only in the browser
+if (typeof window !== 'undefined') {
+    getAnalytics(app);
+}
+
 
 export { app, auth, db };
